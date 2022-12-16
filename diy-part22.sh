371da5ea7aa7297386aebc6c3f7e2a40d5cb7cc9
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2022-12-16 22:54:22 +0800

    Update diy-part22.sh

--- diy-part22.sh
+++ diy-part22.sh
@@ -1,4 +1,7 @@
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell TZ=Asia/Shanghai date +'%Y%m%d-%H%M')' ./include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 sed -i 's/192.168.6.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+
 #sed -i 's/SSID1=Redmi_AX6000_2.4G/SSID1=123123_2.4/g' package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b0.dat
 #sed -i 's/SSID1=Redmi_AX6000_5G/SSID1=123123/g' package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b1.dat
 #sed -i 's/WPAPSK1=88888888/WPAPSK1=88896825/g' package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b0.dat
