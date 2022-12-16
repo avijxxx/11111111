121ff08459201a1077cbbe5a5848a46a31073c18
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2022-12-16 10:50:17 +0800

    Update mt7986.mk

--- diy/target/linux/mediatek/image/mt7986.mk
+++ diy/target/linux/mediatek/image/mt7986.mk
@@ -383,18 +383,18 @@ define Device/xiaomi_redmi-router-ax6000
  TARGET_DEVICES += xiaomi_redmi-router-ax6000
  
 define Device/xiaomi_redmi-router-ax6000-h
-  DEVICE_VENDOR := Xiaomi
-  DEVICE_MODEL := Redmi Router AX6000 H
-  DEVICE_DTS := mt7986a-xiaomi-redmi-router-ax6000-h
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_PACKAGES :=luci-app-mtwifi l1profile wireless-tools kmod-conninfra kmod-warpkmod-mt_wifi ipv6helper kmod-mediatek_hnat bash autocore-arm mtkhnat_util
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 112640k
-  KERNEL_IN_UBI := 1
+   DEVICE_VENDOR := Xiaomi
+   DEVICE_MODEL := Redmi Router AX6000 H
+   DEVICE_DTS := mt7986a-xiaomi-redmi-router-ax6000-h
+   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+   DEVICE_PACKAGES :=luci-app-mtwifi l1profile wireless-tools kmod-conninfra kmod-warpkmod-mt_wifi ipv6helper kmod-mediatek_hnat bash autocore-arm mtkhnat_util
+   UBINIZE_OPTS := -E 5
+   BLOCKSIZE := 128k
+   PAGESIZE := 2048
+   IMAGE_SIZE := 112640k
+   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += xiaomi_redmi-router-ax6000-h
