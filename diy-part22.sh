f1d47f2b26e15d59c6a44b1412757cd8de557e78
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2023-12-08 09:43:05 +0800

    Update diy-part22.sh

--- diy-part22.sh
+++ diy-part22.sh
@@ -33,9 +33,9 @@ EOF
 
 
 #增加alist
-#git clone https://github.com/sbwml/luci-app-alist package/alist
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 
 #增加mosdns 4.5.3
