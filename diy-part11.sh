29c8ad46e78e8031e7d3b90c3ce9543d5228c7d7
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2023-12-11 10:45:31 +0800

    Update diy-part11.sh

--- diy-part11.sh
+++ diy-part11.sh
@@ -1,2 +1,2 @@
 #echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
